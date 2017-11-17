21bad2e3e13146db2cbec87039e95645fa91242f
huangyonghao <huangyonghao@talk-fun.com>
2017-11-17 10:55:45 +0800

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
+    minifycss = require('gulp-minify-css'),
     rename = require('gulp-rename');
 
 gulp.task('less:wxss', function(){
-        gulp.src(['./akj/pages/common/components/*.less'])
-        pipe(less())
-    .pipe(gulp.dest('./akj/pages/common/components/'))
+    gulp.src(['./_kj/pages/common/components/*.less'])
+    .pipe(less())
+    .pipe(minifycss())
+    .pipe(rename({extname: ".wxss"}))
+    .pipe(gulp.dest('./kj/pages/common/components/'))
+})
 
-
+gulp.task('default', function(){
+    gulp.watch('./_kj/pages/common/components/*.less',['less:wxss']);
 })
