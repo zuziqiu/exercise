b9d92a2a26d84105ac9c8128a00dc470a9f6b11b
huangyonghao <huangyonghao@talk-fun.com>
2017-11-23 17:26:03 +0800

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,10 @@ var gulp = require('gulp'),
 
 // less=>min.wxss
 gulp.task('less:wxss', function(){
-    gulp.src(['./_kj/pages/common/components/*.less'])
+    gulp.src(['./_kj/common/components/*.less'])
     .pipe(less())
     .pipe(rename({extname: ".wxss"}))
-    .pipe(gulp.dest('./kj/pages/common/components/'))
+    .pipe(gulp.dest('./kj/common/components/'))
 })
 
 // pages:wxss
@@ -23,17 +23,24 @@ gulp.task('pages:wxss', function(){
     .pipe(less())
     .pipe(rename({extname: ".wxss"}))
     .pipe(gulp.dest('./kj/pages/index/'));
+
+    gulp.src(['./_kj/pages/personal/personal.less'])
+    .pipe(less())
+    .pipe(rename({extname: ".wxss"}))
+    .pipe(gulp.dest('./kj/pages/personal/'));
 })
 
 // wxml
 gulp.task('wxmlmin', function(){
     gulp.src(['./_kj/pages/common/components/*.wxml'])
-    .pipe(less())
     .pipe(gulp.dest('./kj/pages/common/components/'))
 })
 
 gulp.task('default', function(){
-    gulp.watch('./_kj/pages/common/components/*.wxml',['wxmlmin']);
-    gulp.watch('./_kj/pages/common/components/*.less',['less:wxss']);
-    gulp.watch('./_kj/pages/invitation/invitation.less',['pages:wxss']);
+    //gulp.watch('./_kj/common/components/*.wxml',['wxmlmin']);
+    gulp.watch('./_kj/common/components/*.less',['less:wxss']);
+    gulp.watch(['./_kj/pages/invitation/invitation.less',
+                './_kj/pages/index/index.less',
+                './_kj/pages/personal/personal.less'],
+                ['pages:wxss']);
 })
