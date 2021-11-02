ad567f3e6eaa1937125fc97ed0577730233b6725
huangyonghao <huangyonghao@talk-fun.com>
2021-11-03 02:25:04 +0800

    笔记

--- node.js/04_http(s)/server.js
+++ node.js/04_http(s)/server.js
@@ -1,10 +1,12 @@
 const http = require('http')
+const myChunk = require('05_custom_zuziqiu')
 var server = http.createServer((request, response) => {
   let url = request.url
   response.write(url) // 往前端写数据
   response.end() // 没有end ,前端就一直等
 })
 
+console.log(myChunk([4, 5, 6, 7]))
 server.listen(8090, 'localhost', () => {
   console.log('localhost: 8090')
 })
---
+++ node.js/05_custom_zuziqiu/ex.md
@@ -0,0 +1,2 @@
+npm adduser
+npm publish（发布前要确定是npm源而不是淘宝源）
